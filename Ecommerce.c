5eca6aafac509db4b1a5037aaf68412473f45117
Nelson Dutra Junior <juniornd@yahoo.com.br>
2014-11-19 16:41:25 -0200

    Alterações 16:41@19/11

--- Ecommerce.c
+++ Ecommerce.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
+//Estrutura Dimensao
 typedef struct Dimensao{
 	int altura;
 	int largura;
 	int comprimento;
-} Dimensao;
+} DIMENSAO;
 
+//Estrutura Eletro-eletronicos
 typedef struct Eletro{
 	int codigo;
 	char descricao[31];
@@ -15,8 +18,9 @@ typedef struct Eletro{
 	float preco;
 	int estoque;
 	char cor[10];
-} Eletro;
+} ELETRO;
 
+//Estrutura Vestuario
 typedef struct Vestuario{
 	int codigo;
 	char descricao[31];
@@ -25,7 +29,38 @@ typedef struct Vestuario{
 	int estoque;
 	char cor[10];
 	char genero[10];
-} Vestuario;
+} VESTUARIO;
 
+//Estrutura Usuarios
+typedef struct Usuario{
+	int codigo;
+	char nome[16];
+	char ultimoSobrenome[16];
+	char logradouro[51];
+	char numero[8]; //Usei char para numeros como 105A ou 1005-1
+	char complemento[21];
+	int cep;
+	char categoria[8];
+} USUARIO;
+
+//Prototipos das Funções
+
+//Variavel globais
+USUARIO usuarios[200]; //O q vc acha? Quantos usuarios o maximo?
+
+//Funcao main
 int main(){
+}
+
+//Funcao lerUsuariosTexto
+int lerUsuariosTexto(char arq[15]){
+	FILE *fp;
+	char linha[140];//Valor 139 escolhido por ser a soma do tamanho maximo de todos os dados do usuario
+	if((fp = fopen(arq, "r")) == NULL){
+		printf("Erro ao abrir o arquivo %s\n", arq);
+		exit(1);
+	}
+	while(fgets(linha,MAX,fp) != NULL){
+		
+	}
 }
